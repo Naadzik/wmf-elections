e8e78e125c17ea1d90670966099b57f530bc7d82
Maciej Nadzikiewicz <156414425+Naadzik@users.noreply.github.com>
2026-04-20 23:50:23 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/wmf-elections/',
-  server: { port: parseInt(process.env.PORT ?? '5174') },
-})
